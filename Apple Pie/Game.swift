1bfb11be26c20917fb8aef53a85d148c139eb6eb
AlexanderRozhdestvenskiy <alexrozh1988@gmail.com>
2020-08-31 13:13:40 +0300

    1

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,37 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Alexander Rozhdestvenskiy on 23.07.2020.
+//  Copyright © 2020 evilroot. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    var formattedWord: String {
+        var guessWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessWord += "\(letter)"
+            } else {
+                guessWord += ""
+            }
+        }
+        return guessWord
+    }
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+        incorrectMovesRemaining -= 1
+        }
+    }
+}
+
+
+
